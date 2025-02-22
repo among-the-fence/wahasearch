3784a6539f7329d4cd7c1f745c59787a38fcf65e
Bill Hanson <doubleyuhtee@gmail.com>
2025-02-21 22:04:05 -0600

    working?

--- wahasearch/src/App.tsx
+++ wahasearch/src/App.tsx
@@ -11,12 +11,16 @@ import { GameData, WBSDataGameSystemParser } from './lib/gameData';
       new WBSDataGameSystemParser().parseGameSystem().then(c => setParsedData(c));
     }, []);
   
+    const units = parsedData?.catalogues?.map(c => 
+      c.selectionEntries).flat();
+      // console.log(parsedData?.catalogues.map(c=> c.selectionEntries).flat(), units);
 
   return (
     <>
       <div className='mt-12'>
         <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 mt-5 lg:mt-0 gap-4">
-          {parsedData?.selections?.map(unit => (
+          {units?.map(unit => (
+            unit && 
             <h1 style={{color:'white'}} key={unit.id}  > {unit.name} </h1>
           ))}
         </div>
