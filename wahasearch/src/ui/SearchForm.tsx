4404325fe89de0d5830769175a404f7cbb1c02c8
Bill Hanson <doubleyuhtee@gmail.com>
2025-06-23 13:02:11 -0500

    wtf am i doing

--- wahasearch/src/ui/SearchForm.tsx
+++ wahasearch/src/ui/SearchForm.tsx
@@ -1,13 +1,13 @@
 import { FILTERS_KEYWORDS, FILTERS_LEGENDS, LEGENDS_ALL, LEGENDS_NONE, LEGENDS_ONLY } from "@/lib/constants";
-import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { ChangeEvent, useEffect, useState } from "react";
 
 export interface SearchFormProps {
     applyFunction: (form: Map<String, String>) => void;
 }
 
-export const SearchForm = ({applyFunction}: SearchFormProps) => {
-  const [localFormState, setLocalFormState] = useState(new Map<String, String>());
+export const SearchForm = ({ applyFunction }: SearchFormProps) => {
+    const [localFormState, setLocalFormState] = useState(new Map<String, String>());
 
     useEffect(() => {
         const timeOutId = setTimeout(() => applyFunction(localFormState), 500);
@@ -29,7 +29,7 @@ export const SearchForm = ({applyFunction}: SearchFormProps) => {
         if (value.length >= 2) {
             localFormState.set(FILTERS_LEGENDS, LEGENDS_ALL);
         }
-        else if (value.length == 0 || value.includes("current")){
+        else if (value.length == 0 || value.includes("current")) {
             localFormState.set(FILTERS_LEGENDS, LEGENDS_NONE);
         }
         else {
@@ -55,10 +55,10 @@ export const SearchForm = ({applyFunction}: SearchFormProps) => {
             <div>
                 <ToggleGroup defaultValue={["current"]} className="col-span-3" type="multiple" onValueChange={e => updateLegendsState(e)}>
                     <ToggleGroupItem className="border-2" defaultChecked={true} value="current" aria-label="Toggle current">
-                    Current
+                        Current
                     </ToggleGroupItem>
                     <ToggleGroupItem className="border-2" value="legends" aria-label="Toggle legends">
-                    Legends
+                        Legends
                     </ToggleGroupItem>
                 </ToggleGroup>
             </div>
